2112ef2fc1e20e9d2ade2eae4df52c6c725a0066
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-01-31 23:58:38 +0600

    conditional return two example added

--- conditional-return.js
+++ conditional-return.js
@@ -7,3 +7,17 @@ function oddNumber(num){
 }
 console.log(oddNumber(10));
 console.log(oddNumber(9));
+
+
+// *******************
+
+function checkkor(number, doubble){
+    if(doubble === true){
+        const result = number *2;
+        return result;
+    }else{
+        const result = number * 0;
+        return result;
+    }
+}
+console.log(checkkor(5,false));
