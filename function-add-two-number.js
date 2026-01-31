2cb79e322d5e4958fcee0f1d843443aa6bee85ce
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-01-31 22:04:11 +0600

    add javascript function

---
+++ function-add-two-number.js
@@ -0,0 +1,13 @@
+function sum(num1,num2){
+    const add = num1+num2;
+    console.log(add);
+}
+sum(10,2);
+
+
+// squire a number
+function number(num){
+    const squire = num*num;
+    console.log(squire);
+}
+number(5);
