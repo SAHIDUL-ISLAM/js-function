6a387ad70ae47e7345aaa5764112a843c53bf140
sahidul-islam <sahidul99622@gmail.com>
2026-02-03 15:08:58 +0600

    get minimum price from an phonre price array

--- arr-max-value.js
+++ arr-max-value.js
@@ -12,19 +12,35 @@
 // const array = arrNumbers(arr);
 // console.log(array);
 
+// max value 
+
+// const numbers = [15,85,4,15,17,88,25,36,19,82,48,24,6,99];
+
+// function maxArrayNumbers(numbers){
+//     let num = numbers[0];
+//     for(const array of numbers){
+//         if(array>num){
+//             num = array;
+//         }
+//     }
+//     return num;
+// }
+// const result = maxArrayNumbers(numbers);
+// console.log(result);
 
 
-const numbers = [15,85,4,15,17,88,25,36,19,82,48,24,6,99];
+// ********************
+// min value  
 
-function maxArrayNumbers(numbers){
-    let num = numbers[0];
-    for(const array of numbers){
-        if(array>num){
-            num = array;
+function minimumPrice(amount){
+    let min = amount[0];
+    for(const price of amount){
+        if(price<min){
+            min = price;
         }
     }
-    return num;
+    return min;
 }
-const result = maxArrayNumbers(numbers);
+const array = [19000,16000,20000,25000,13000,15000];
+const result = minimumPrice(array);
 console.log(result);
-
